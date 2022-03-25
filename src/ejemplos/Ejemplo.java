05116672adf5c8b4137cfdfa50eedbdc67b3077b
PTC-ISC <PTC-ISC@AIO-PTC-ISC>
2022-03-25 15:17:48 -0700

    Se agrego linea

--- src/ejemplos/Ejemplo.java
+++ src/ejemplos/Ejemplo.java
@@ -2,4 +2,10 @@ package ejemplos;
 
 public class Ejemplo {
   //hola mundo
+	
+	public static void main(String[] args) {
+		System.out.print("Hola Mundo"); 
+		System.out.print("Agregar Line"); 
+		
+	}
 }
